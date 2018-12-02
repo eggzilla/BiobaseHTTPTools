e7245cd01e96c107244b28a2a474b7b8495c4e1f
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-03 00:31:43 +0100

    Added ci testing

---
+++ Biobase/LocusTagToUniProt.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE Arrows #-}
+{-# LANGUAGE RecordWildCards #-}
+module Main where
+
+import System.Console.CmdArgs
+import Biobase.Entrez.HTTP
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.List
+import Data.Maybe
+
+options :: Options
+data Options = Options
+  { accession :: String
+  } deriving (Show,Data,Typeable)
+
+options = Options
+  {  = def &= name "a" &= help "NCBI accession number, e.g b0001"
+  } &= summary ("LocusTagToUniProt") &= help "Florian Eggenhofer - 2018" &= verbosity
+
+accessionToTaxId :: String -> IO ()
+accessionToTaxId _accession = do
+  let program = Just "esummary"
+  let database = Just "nucleotide"
+  let queryString = "id=" ++ _accession
+  let entrezQuery = EntrezHTTPQuery program database queryString
+  result <- entrezHTTP entrezQuery
+  let summary = head (readEntrezSummaries result)
+  let taxIdItem = find (\a -> itemName a == "TaxId") (summaryItems (head (documentSummaries summary)))
+  putStrLn (itemContent (fromJust taxIdItem))
+
+main = do
+  Options{..} <- cmdArgs options
+  accessionToTaxId accession
