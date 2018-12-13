c87f7a3401c2cd54ce8b618d443f820dd33907a8
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-13 18:58:55 +0100

    Enabled geneid input from file, updated documentation

--- Biobase/GeneIdToUniProtId.hs
+++ Biobase/GeneIdToUniProtId.hs
@@ -5,21 +5,22 @@ module Main where
 
 import System.Console.CmdArgs
 import Biobase.Ensembl.HTTP
---import Data.List
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 options :: Options
 data Options = Options
-  { geneIds :: String
+  { geneIds :: String,
+    geneIdFilePath :: String
   } deriving (Show,Data,Typeable)
 
 options = Options
-  { geneIds = def &= name "g" &= help "Ensembl gene ids (or NCBI locus tags) comma separated, e.g b0001,b0825"
+  { geneIds = def &= name "g" &= help "Ensembl gene ids (or NCBI locus tags) comma separated, e.g b0001,b0825",
+    geneIdFilePath = def &= name "f" &= help "Path to file containing one Ensembl gene ids (or NCBI locus tags) per line, e.g b0001\nb0825\n"    
   } &= summary ("GeneIdToUniProtId") &= help "Florian Eggenhofer - 2018" &= verbosity
 
-geneIdToUniProtId :: T.Text -> IO ()
-geneIdToUniProtId geneIdsText = do
-  let geneIdList = T.splitOn "," geneIdsText
+geneIdToUniProtId :: [T.Text] -> IO ()
+geneIdToUniProtId geneIdList = do
   uniprotIds <- requestUniProtWithGeneIds geneIdList
   let output = map (\(a,b) -> a `T.append` (T.pack "\t") `T.append` b) uniprotIds
   mapM_ (putStrLn . T.unpack) output
@@ -27,4 +28,12 @@ geneIdToUniProtId geneIdsText = do
 main :: IO ()
 main = do
   Options{..} <- cmdArgs options
-  geneIdToUniProtId (T.pack geneIds)
+  let geneIdsText = T.pack geneIds
+  let geneIdList = T.splitOn "," geneIdsText
+  if null geneIdFilePath
+    then geneIdToUniProtId geneIdList
+    else do
+      geneIdFile <- TIO.readFile geneIdFilePath
+      let fileGeneIds = T.lines geneIdFile
+      let combinedGeneIds = geneIdList ++ fileGeneIds
+      geneIdToUniProtId combinedGeneIds
--- GeneIdToGOTerms.md
+++ GeneIdToGOTerms.md
@@ -1,13 +1,20 @@
 GeneIdToGOTerms
 
 options [OPTIONS]
+
   Florian Eggenhofer - 2018
 
 Common flags:
-  -g --geneids=ITEM  Ensembl gene ids (or NCBI locus tags) comma separated,
-                     e.g b0001,b0825
-  -? --help          Display help message
-  -V --version       Print version information
-  -v --verbose       Loud verbosity
-  -q --quiet         Quiet verbosity
+
+  -g --geneids=ITEM         Ensembl gene ids (or NCBI locus tags) comma separated, e.g b0001,b0825
+
+  -f --geneidfilepath=ITEM  Path to file containing one Ensembl gene ids (or NCBI locus tags) per line, e.g b0001 b0825
+
+  -? --help                 Display help message
+
+  -V --version              Print version information
+
+  -v --verbose              Loud verbosity
+
+  -q --quiet                Quiet verbosity
 
