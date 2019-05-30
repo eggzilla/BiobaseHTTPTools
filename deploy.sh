8af79c6681d795d7c03c074ddeff60c90fbf8e2c
Florian Eggenhofer <egg@tbi.univie.ac.at>
2019-05-30 17:09:06 +0200

    Added verbose output

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "docker ps -a"
+docker ps -a
 export DOCKERID=`docker ps -alq`
+echo "docker cp $DOCKERID:/source/BiobaseHTTPTools.tar.gz ."
 docker cp $DOCKERID:/source/BiobaseHTTPTools.tar.gz .
